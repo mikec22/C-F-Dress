350c43694f2adb88bbb1157d003290893987a4f7
mikec22 <Mike@Mikes-MacBook-Pro.local>
2016-11-20 01:50:51 +0800

    test

--- src/java/com/bean/Staff.java
+++ src/java/com/bean/Staff.java
@@ -41,20 +41,20 @@ public class Staff {
         this.password = password;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getGender() {
         return gender;
     }
 
     public void setGender(String gender) {
         this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
 }
