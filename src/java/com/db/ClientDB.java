da36580b2ec69ca4658efaa8bdecc6b1c1b02ce9
mikec22 <Mike@Mikes-MacBook-Pro.local>
2016-11-20 04:21:46 +0800

    change project name

---
+++ src/java/com/db/ClientDB.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Mike
+ */
+public class ClientDB {
+    String dburl, dbUser, dbPassword;
+
+    public ClientDB() {
+    }
+
+    public ClientDB(String dburl, String dbUser, String dbPassword) {
+        this.dburl = dburl;
+        this.dbUser = dbUser;
+        this.dbPassword = dbPassword;
+    }
+    
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection(dburl, dbUser, dbPassword);
+        } catch (ClassNotFoundException e) {
+           e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public boolean isValidUser(String login_id, String pwd) {
+        Connection cnnct = null;
+        PreparedStatement pStmnt = null;
+        boolean isValid = false;
+        try {
+            cnnct = getConnection();
+            String preQueryStatement = "SELECT * FROM client WHERE login_id=? and password=?";
+            pStmnt = cnnct.prepareStatement(preQueryStatement);
+            pStmnt.setString(1, login_id);
+            pStmnt.setString(2, pwd);
+            ResultSet rs = null;
+            rs = pStmnt.executeQuery();
+            if (rs.next()) {
+                isValid = true;
+            }
+            pStmnt.close();
+            cnnct.close();
+
+        } catch (SQLException ex) {
+            while (ex != null) {
+                ex.printStackTrace();
+                ex = ex.getNextException();
+            }
+        }
+        return isValid;
+    }
+}
--- src/java/com/servlet/LoginController.java
+++ src/java/com/servlet/LoginController.java
@@ -5,17 +5,23 @@
  */
 package com.servlet;
 
+import com.bean.Client;
+import com.db.ClientDB;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author Mike
  */
+@WebServlet(name = "LoginController", urlPatterns = {"/main"})
 public class LoginController extends HttpServlet {
 
     /**
@@ -27,6 +33,67 @@ public class LoginController extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    private ClientDB db;
+
+    @Override
+    public void init() throws ServletException {
+        super.init(); //To change body of generated methods, choose Tools | Templates.
+        String dbUrl = this.getServletContext().getInitParameter("dbUrl");
+        String dbUser = this.getServletContext().getInitParameter("dbUser");
+        String dbPassword = this.getServletContext().getInitParameter("dbPassword");
+        db = new ClientDB(dbUrl, dbUser, dbPassword);
+    }
+
+    private void doAuthenticate(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String targetURL;
+        boolean isValid = db.isValidUser(username, password);
+        if (isValid) {
+            HttpSession session = request.getSession(true);
+            Client client = new Client();
+            client.setLogin_id(username);
+            client.setPassword(password);
+            
+
+            session.setAttribute("userInfo", client);
+            targetURL = "/welcome.jsp";
+        } else {
+            targetURL = "/loginError.jsp";
+        }
+        RequestDispatcher rd;
+        rd = getServletContext().getRequestDispatcher("/" + targetURL);
+        rd.forward(request, response);
+    }
+
+    private boolean isAuthenticated(HttpServletRequest request) {
+        boolean result = false;
+        HttpSession session = request.getSession();
+        if (session.getAttribute("userInfo") != null) {
+            result = true;
+        }
+        return result;
+    }
+
+    private void doLogin(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String targetURL = "login.jsp";
+        RequestDispatcher rd;
+        rd = getServletContext().getRequestDispatcher("/" + targetURL);
+        rd.forward(request, response);
+    }
+
+    private void doLogout(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.removeAttribute("userInfo");
+            session.invalidate();
+        }
+        doLogin(request, response);
+    }
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -35,7 +102,7 @@ public class LoginController extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginController</title>");            
+            out.println("<title>Servlet LoginController</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet LoginController at " + request.getContextPath() + "</h1>");
@@ -56,7 +123,7 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        doPost(request, response);
     }
 
     /**
@@ -70,7 +137,19 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String action = request.getParameter("action");
+        if (!isAuthenticated(request)
+                && !("authenticate".equals(action))) {
+            doLogin(request, response);
+            return;
+        }
+        if ("authenticate".equals(action)) {
+            doAuthenticate(request, response);
+        } else if ("logout".equals(action)) {
+            doLogout(request, response);
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED);
+        }
     }
 
     /**
