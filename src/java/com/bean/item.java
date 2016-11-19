51d3dd53e3455a76e738f16b0fe0bd6b4555cbfa
mikec22 <Mike@Mikes-MacBook-Pro.local>
2016-11-20 01:06:50 +0800

    some bean

--- src/java/com/bean/item.java
+++ src/java/com/bean/item.java
@@ -10,5 +10,60 @@ package com.bean;
  * @author Mike
  */
 public class item {
-    
+
+    private int item_id;
+    private String name, category, designer, description;
+    private double price;
+
+    public item() {
+    }
+
+    public int getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(int item_id) {
+        this.item_id = item_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDesigner() {
+        return designer;
+    }
+
+    public void setDesigner(String designer) {
+        this.designer = designer;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 }
