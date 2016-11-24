a5db28b7b4edf725c9bff1b198513aa7ae93dc71
sywsyw <shukyanw@gmail.com>
2016-11-25 03:44:27 +0800

    date

--- src/java/com/servlet/HandleOrderController.java
+++ src/java/com/servlet/HandleOrderController.java
@@ -5,15 +5,11 @@ import com.bean.Order;
 import com.bean.OrderLine;
 import com.db.OrderDB;
 import java.io.IOException;
-import java.sql.Date;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.Calendar;
+import java.sql.Date;
 import java.util.Vector;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -52,52 +48,57 @@ public class HandleOrderController extends HttpServlet {
         HttpSession session = request.getSession();
         String action = request.getParameter("action");
         String msg = "";
-        if (action.equals("placeOrder")) {
-     
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        Calendar calobj = Calendar.getInstance();
-        System.out.println("I AM HERE TODAY" + df.format(calobj.getTime()));
-        String datetime = request.getParameter("test");
-        System.out.println("DATE: "+datetime);
-                System.out.println("I AM HERE TODAY" + df.format(datetime));
-        //Date delivery_datetime = java.sql.Date.valueOf(request.getParameter("delivery_datetime"));
-        String delivery_datetime = request.getParameter("delivery_date") + " " + request.getParameter("delivery_time") + ":00";
-        System.out.println("delivery_datetime:  " + delivery_datetime);
-//        String delivery_datetime = "1999-05-05 13:44:00";
-        String option = request.getParameter("delivery_method");
-        System.out.println("I AM HERE  " + option);
-        Client client = (Client) session.getAttribute("clientInfo");
-        int client_id = client.getClient_id();
-        String address = request.getParameter("delivery_address");
-        String status = "processing";
-        Order order = (Order) session.getAttribute("cart");
-        System.out.println("I AM HERE ID: " + client_id);
-        double amount = order.getTotalPrice();
-        double balance = client.getBalance();
-        double credit = client.getCredit_amount();
-        System.out.println("I AM HERE A: " + amount + " B: " + balance + " C: " + credit);
-        if ((balance + credit) < amount) {
-            msg = "Balance not enough ";
-        } else {
-            Vector<OrderLine> order_line = order.getOrder_lines();
-            System.out.println("I AM HERE" + order_line);
-            boolean add = orderDB.addOrder(client_id, delivery_datetime, address, option, status, order_line);
-            System.out.println("I AM HERE AFTER ADD" + add);
-            if (add) {
-                int bonus_point = client.getBonus_point();
-                if (amount > 2000) {
-                    bonus_point += (int) (amount * 0.05);
-                }
-                msg = "success";
-            } else {
-                msg = "fail";
-            }
-        }
+//        if (action.equals("placeOrder")) {
 
-        System.out.println("Message:   " + msg);
+            DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Calendar c = Calendar.getInstance();
+
+// set the calendar to start of today
+            c.set(Calendar.HOUR_OF_DAY, 0);
+            c.set(Calendar.MINUTE, 0);
+            c.set(Calendar.SECOND, 0);
+            c.set(Calendar.MILLISECOND, 0);
+
+// and get that as a Date
+            Date today = new java.sql.Date(Calendar.getInstance().getTimeInMillis());
+            Date delivery_datetime = java.sql.Date.valueOf(request.getParameter("delivery_date")
+                    + " " + request.getParameter("delivery_time") + ":00");
+
+            boolean isVaild = today.compareTo(delivery_datetime) == 1;
+
+            String option = request.getParameter("delivery_method");
+            Client client = (Client) session.getAttribute("clientInfo");
+            int client_id = client.getClient_id();
+            String address = request.getParameter("delivery_address");
+            String status = "processing";
+            Order order = (Order) session.getAttribute("cart");
+            System.out.println("I AM HERE ID: " + client_id);
+            double amount = order.getTotalPrice();
+            double balance = client.getBalance();
+            double credit = client.getCredit_amount();
+            System.out.println("I AM HERE A: " + amount + " B: " + balance + " C: " + credit);
+            if ((balance + credit) < amount) {
+                msg = "Balance not enough ";
+            } else {
+                Vector<OrderLine> order_line = order.getOrder_lines();
+                System.out.println("I AM HERE" + order_line);
+                boolean add = orderDB.addOrder(client_id, delivery_datetime, address, option, status, order_line);
+                System.out.println("I AM HERE AFTER ADD" + add);
+                if (add) {
+                    int bonus_point = client.getBonus_point();
+                    if (amount > 2000) {
+                        bonus_point += (int) (amount * 0.05);
+                    }
+                    msg = "success";
+                } else {
+                    msg = "fail";
+                }
+            }
+
+            System.out.println("Message:   " + msg);
         }
 //        String targetUrl = "/placeOrder.jsp";
 //        response.sendRedirect(targetUrl);
 //        }
-    }
+//    }
 }
