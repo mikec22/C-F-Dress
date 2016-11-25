67c3516c799ede8c3d3b0cd3b1273a1d0a8a2498
sywsyw <shukyanw@gmail.com>
2016-11-25 10:41:24 +0800

    complete option

--- src/java/com/servlet/HandleOrderController.java
+++ src/java/com/servlet/HandleOrderController.java
@@ -65,31 +65,35 @@ public class HandleOrderController extends HttpServlet {
         c.set(Calendar.MILLISECOND, 0);
         c.add(Calendar.DAY_OF_WEEK, -1);
 
-// and get that as a Date
-//        Date today = new java.sql.Date(Calendar.getInstance().getTimeInMillis());
-//        Date delivery_date =  java.sql.Date.valueOf(request.getParameter("delivery_date"));
-//        Date delivery_datetime = java.sql.Date.valueOf(datetime);
         boolean isVaild = false;
-        String datetime = request.getParameter("delivery_date")
-                + " " + request.getParameter("delivery_time") + ":00";
-        java.sql.Timestamp delivery_datetime = java.sql.Timestamp.valueOf(datetime);
-        java.util.Date today = c.getTime();
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        java.util.Date delivery_date;
-        try {
-            delivery_date = df.parse(request.getParameter("delivery_date"));
-            isVaild = delivery_date.compareTo(today) == 1;
-
-        } catch (ParseException ex) {
-            Logger.getLogger(HandleOrderController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+        //default 1999-01-01 01:01:01;
+        String datetime = "1999-01-01 01:01:01";
+        String address = "";
         String option = request.getParameter("delivery_method");
+        if (option.equals("delivery")) {
+            datetime = request.getParameter("delivery_date")
+                    + " " + request.getParameter("delivery_time") + ":00";
+            java.sql.Timestamp delivery_datetime = java.sql.Timestamp.valueOf(datetime);
+            java.util.Date today = c.getTime();
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+            java.util.Date delivery_date;
+            try {
+                delivery_date = df.parse(request.getParameter("delivery_date"));
+                isVaild = delivery_date.compareTo(today) == 1;
+
+            } catch (ParseException ex) {
+                Logger.getLogger(HandleOrderController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        } else if (option.equals("self")) {
+            address = "self pick-up";
+            isVaild = true;
+        }
         Client client = (Client) session.getAttribute("clientInfo");
         client = clientDB.getClient(client.getClient_id());
         session.setAttribute("clientInfo", client);
         int client_id = client.getClient_id();
-        String address = request.getParameter("delivery_address");
+
         String status = "processing";
         Order order = (Order) session.getAttribute("cart");
         double amount = order.getTotalPrice();
@@ -99,39 +103,37 @@ public class HandleOrderController extends HttpServlet {
         int client_dp = client.getBonus_point();
         if (!isVaild) {
             msg = "The delivery date Can not be greater than the current time";
+        } else if (((balance + credit) < amount) && (dp > client_dp)) {
+            msg = "Balance and Bonus Points not enough ";
+        } else if ((balance + credit) < amount) {
+            msg = "Balance not enough ";
+        } else if ((dp > client_dp)) {
+            msg = "Bonus Points not enough ";
         } else {
-            if (((balance + credit) < amount) && (dp > client_dp)) {
-                msg = "Balance and Bonus Points not enough ";
-            } else if ((balance + credit) < amount) {
-                msg = "Balance not enough ";
-            } else if ((dp > client_dp)) {
-                msg = "Bonus Points not enough ";
-            } else {
-                Vector<OrderLine> order_line = order.getOrder_lines();
-                boolean add = orderDB.addOrder(client_id, datetime, address, option, status, order_line);
-                if (add) {
-                    int bonus_point = client.getBonus_point();
-                    if (amount > 2000) {
-                        bonus_point += (int) (amount * 0.05);
-                    }
-                    balance -= amount;
-                    bonus_point -= dp;
-                    client.setBalance(balance);
-                    client.setBonus_point(bonus_point);
-                    if (clientDB.updateClientBalanceBonusPoints(client)) {
-                        msg = "success";
-                        Order orderResult = new Order();
-                        orderResult.setOrder_lines(order.getOrder_lines());
-                        order = new Order();
-                        client = clientDB.getClient(client.getClient_id());
-                        session.setAttribute("clientInfo", client);
-                        session.setAttribute("cart", order);
-                        request.setAttribute("orderResult", orderResult);
-                        getServletContext().getRequestDispatcher("/orderSuccess.jsp").forward(request, response);
-                    }
-                } else {
-                    msg = "fail";
+            Vector<OrderLine> order_line = order.getOrder_lines();
+            boolean add = orderDB.addOrder(client_id, datetime, address, option, status, order_line);
+            if (add) {
+                int bonus_point = client.getBonus_point();
+                if (amount > 2000) {
+                    bonus_point += (int) (amount * 0.05);
                 }
+                balance -= amount;
+                bonus_point -= dp;
+                client.setBalance(balance);
+                client.setBonus_point(bonus_point);
+                if (clientDB.updateClientBalanceBonusPoints(client)) {
+                    msg = "success";
+                    Order orderResult = new Order();
+                    orderResult.setOrder_lines(order.getOrder_lines());
+                    order = new Order();
+                    client = clientDB.getClient(client.getClient_id());
+                    session.setAttribute("clientInfo", client);
+                    session.setAttribute("cart", order);
+                    request.setAttribute("orderResult", orderResult);
+                    getServletContext().getRequestDispatcher("/orderSuccess.jsp").forward(request, response);
+                }
+            } else {
+                msg = "fail";
             }
         }
 
